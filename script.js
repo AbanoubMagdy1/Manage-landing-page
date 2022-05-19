8c1d8854cd7e0f62172909f24547e700132d984c
AbanoubMagdy1 <bebomagdy666@gmail.com>
2022-05-19 14:22:19 +0200

    Add mobile menu, hamburger and overlay

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu');
+const overlay = document.querySelector('.overlay');
+
+hamburger.addEventListener('click', () => {
+	document.body.classList.add('open')
+	hamburger.classList.add('open')
+	menu.classList.add('open')
+	overlay.classList.add('open')
+})
+
+overlay.addEventListener('click', () => {
+	document.body.classList.remove('open')
+	hamburger.classList.remove('open')
+	menu.classList.remove('open')
+	overlay.classList.remove('open')
+})
+
 new Glide('.glide', {
 	type: 'carousel',
 	gap: 30,
